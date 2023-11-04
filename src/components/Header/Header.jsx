a59675701a5b49d1142a156a016c4402f78c398c
Mahin-Hasan <mahinmtrs@gmail.com>
2023-11-04 20:48:23 +0600

    added dynamic posts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
@@ -8,8 +8,10 @@ const Header = () => {
             <nav>
                 <span>My Website</span>
                 <Link to="/">Home</Link>
-                <Link to="/users">Users</Link>
-                <Link to="/about">About</Link>
+                <NavLink to="/users">Users</NavLink>
+                {/* <Link to="/users">Users</Link> */}
+                <NavLink to="/posts">Posts</NavLink>
+                <NavLink to="/about">About</NavLink>
                 <Link to="/contact">Contact Us</Link>
 
             </nav>
